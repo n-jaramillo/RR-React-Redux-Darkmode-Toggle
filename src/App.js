5aea1d3a7267b15ebea16d92b9b38ca5e77b3d38
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-03-29 03:47:48 -0700

    adding redux and toggleMode to App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleMode } from './features/modeSlice'
 
 function App() {
-  
+  const mode = useSelector((state) => state.mode)
+  const dispatch = useDispatch()
+
+  const handleMode = () => {
+    dispatch(toggleMode())
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
-      <ContentWrapper />
-      <Footer />
+    <div style={{ backgroundColor: mode.color1, color: `${mode.darkMode ? 'white' : 'black'}`, margin: '10px', padding: '10px' }} className="App">
+      <Nav mode={mode} />
+      <button onClick={() => handleMode()} style={{margin: "20px 0"}}>{mode.darkMode ? 'Light Mode' : 'Dark Mode'}</button>
+      <ContentWrapper mode={mode} />
+      <Footer mode={mode} />
     </div>
   );
 }
